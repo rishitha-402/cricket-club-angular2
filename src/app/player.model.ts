1d0fd1323052921f7d0aef69cffef853fdad6d9e
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-16 10:01:31 -0700

    Add a player model with properties

---
+++ src/app/player.model.ts
@@ -0,0 +1,11 @@
+//******* Player object with properties
+
+export class Player {
+  constructor (
+    public name: string,
+    public age: number,
+    public hand: string,
+    public role: string,
+    public id: number
+  ) {}
+}
